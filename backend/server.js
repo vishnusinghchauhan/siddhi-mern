9738b60cc2673cc6b057ac4f9f2faaaad6922e4f
Ram Gopal <ramgopal1291@gmail.com>
2024-12-07 16:17:15 +0530

    dep

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,24 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+
+app.get('/', (req, res) => {
+    res.send('Server is Running! 🚀');
+});
+
+
 // deployment
-__dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send('Server is Running! 🚀');
-    });
-}
+// __dirname = path.resolve();
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+//     });
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Server is Running! 🚀');
+//     });
+// }
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
